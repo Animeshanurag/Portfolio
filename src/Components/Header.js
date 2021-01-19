66cbe2fd673bb9e352cacfc9f65a58861628f0d5
animeshanurag12 <animeshanurag12@gmail.com>
2021-01-19 20:37:38 +0530

    Projects and messages

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,8 +5,6 @@ class Header extends Component {
   render() {
 
     if(this.props.data){
-       var project = this.props.data.project;
-       var github = this.props.data.github;
       var name = this.props.data.name;
       var description= this.props.data.description;
       var city= this.props.data.address.city;
@@ -37,10 +35,6 @@ class Header extends Component {
             <h1 className="responsive-headline">{name}</h1>
             <h3>{description}.</h3>
             <hr />
-            {/* <ul className="social">
-               <a href={project} className="button btn project-btn"><i className="fa fa-book"></i>Project</a>
-               <a href={github} className="button btn github-btn"><i className="fa fa-github"></i>Github</a>
-            </ul> */}
          </div>
       </div>
 
